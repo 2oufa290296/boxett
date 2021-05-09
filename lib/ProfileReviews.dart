12ec21a6a433d2a4be646dc4f3cbbf94e2bada32
2oufa290296 <83858799+2oufa290296@users.noreply.github.com>
2021-05-09 08:16:38 +0200

    Update ProfileReviews.dart

--- lib/ProfileReviews.dart
+++ lib/ProfileReviews.dart
@@ -81,163 +81,165 @@ class _ProfileReviewsState extends State<ProfileReviews> {
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      appBar: PreferredSize(
-          child: Container(
-              height: 50,
-              margin: EdgeInsets.only(top: 30),
-              child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-                InkWell(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                      margin: EdgeInsets.only(left: 20),
-                      child: Icon(Icons.arrow_back_ios, color: Colors.white)),
-                ),
-                Container(
-                    margin: EdgeInsets.only(left: 15),
-                    child: Text('Your Reviews',
-                        style: TextStyle(color: Colors.white, fontSize: 20,fontFamily:"Lobster",letterSpacing:1)))
-              ]),
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromRGBO(18,42,76,1),
-                  Color.fromRGBO(5,150,197,1),
-                  Color.fromRGBO(18,42,76,1)
-                ],
-              ))),
-          preferredSize: Size(width, 50)),
-      body: showProgressBar
-          ? Center(child:  Container(
-                    width: 50,
-                    height: 50,
-                    child: FlareActor(
-                      'assets/loading.flr',
-                      animation: 'Loading',
-                    ))):
-          emptyRev?Container(alignment:Alignment.center,height:height-100,
-                  child: Text("You haven't reviewed any gift yet",textAlign: TextAlign.center,style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 18,
-                                   ),),
-                ): Container(
-              child: ListView.builder(
-                  itemCount: reviewsList.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      elevation:5,
-                      margin: EdgeInsets.only(
-                          top: 5, right: 0.01 * width, left: 0.01 * width),
-                      color: Color(0xFF181818),
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => new ShowReview(
-                                      reviewsList[index].orderId,
-                                      reviewsList[index].profilepic,
-                                      reviewsList[index].giftId,
-                                      reviewsList[index].onTime,
-                                      reviewsList[index].goodPackaging,
-                                      reviewsList[index].materialQuality,
-                                      reviewsList[index].worthPrice,
-                                      reviewsList[index].review)));
-                        },
-                        child: Container(
-                          padding: EdgeInsets.only(
-                              top: 5, bottom: 5, left: 5, right: 5),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Card(color:Colors.transparent,
-                                clipBehavior: Clip.antiAlias,
-                                child: Container(
-                                  width: width * 0.2,
-                                  height: width * 0.2,
-                                  child: CachedNetworkImage(
-                                    imageUrl: reviewsList[index].profilepic,
-                                    fit: BoxFit.cover,
-                                    height: width * 0.2,
+    return SafeArea(
+          child: Scaffold(
+        appBar: PreferredSize(
+            child: Container(
+                height: 50,
+                margin: EdgeInsets.only(top: 30),
+                child: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        child: Icon(Icons.arrow_back_ios, color: Colors.white)),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(left: 15),
+                      child: Text('Your Reviews',
+                          style: TextStyle(color: Colors.white, fontSize: 20,fontFamily:"Lobster",letterSpacing:1)))
+                ]),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromRGBO(18,42,76,1),
+                    Color.fromRGBO(5,150,197,1),
+                    Color.fromRGBO(18,42,76,1)
+                  ],
+                ))),
+            preferredSize: Size(width, 50)),
+        body: showProgressBar
+            ? Center(child:  Container(
+                      width: 50,
+                      height: 50,
+                      child: FlareActor(
+                        'assets/loading.flr',
+                        animation: 'Loading',
+                      ))):
+            emptyRev?Container(alignment:Alignment.center,height:height-100,
+                    child: Text("You haven't reviewed any gift yet",textAlign: TextAlign.center,style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 18,
+                                     ),),
+                  ): Container(
+                child: ListView.builder(
+                    itemCount: reviewsList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        elevation:5,
+                        margin: EdgeInsets.only(
+                            top: 5, right: 0.01 * width, left: 0.01 * width),
+                        color: Color(0xFF181818),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => new ShowReview(
+                                        reviewsList[index].orderId,
+                                        reviewsList[index].profilepic,
+                                        reviewsList[index].giftId,
+                                        reviewsList[index].onTime,
+                                        reviewsList[index].goodPackaging,
+                                        reviewsList[index].materialQuality,
+                                        reviewsList[index].worthPrice,
+                                        reviewsList[index].review)));
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(
+                                top: 5, bottom: 5, left: 5, right: 5),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Card(color:Colors.transparent,
+                                  clipBehavior: Clip.antiAlias,
+                                  child: Container(
                                     width: width * 0.2,
-                                    progressIndicatorBuilder:
-                                        (context, url, progress) {
-                                      return Shimmer.fromColors(
-                                        enabled: true,
-                                        child: Container(
-                                            height: width * 0.2,
-                                            width: width * 0.2,
-                                            color: Color(0xFF282828)),
-                                        baseColor: Color(0xFF282828),
-                                        highlightColor: Color(0xFF383838),
-                                      );
-                                    },
+                                    height: width * 0.2,
+                                    child: CachedNetworkImage(
+                                      imageUrl: reviewsList[index].profilepic,
+                                      fit: BoxFit.cover,
+                                      height: width * 0.2,
+                                      width: width * 0.2,
+                                      progressIndicatorBuilder:
+                                          (context, url, progress) {
+                                        return Shimmer.fromColors(
+                                          enabled: true,
+                                          child: Container(
+                                              height: width * 0.2,
+                                              width: width * 0.2,
+                                              color: Color(0xFF282828)),
+                                          baseColor: Color(0xFF282828),
+                                          highlightColor: Color(0xFF383838),
+                                        );
+                                      },
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  children: <Widget>[
-                                    Container(
-                                      padding: EdgeInsets.only(left: 5, top: 5),
-                                      child: Row(
-                                        
-                                        children: <Widget>[
-                                          Container(
-                                            child: SmoothStarRating(
-                                              allowHalfRating: true,
-                                              starCount: 5,
-                                              rating: reviewsList[index].rate,
-                                              size: 18,
-                                              spacing: 2,
-                                              isReadOnly: true,
-                                              defaultIconData: Icons.star,
-                                              borderColor: Color(0xFF484848),
-                                              color: Colors.yellow[700],
+                                Expanded(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Container(
+                                        padding: EdgeInsets.only(left: 5, top: 5),
+                                        child: Row(
+                                          
+                                          children: <Widget>[
+                                            Container(
+                                              child: SmoothStarRating(
+                                                allowHalfRating: true,
+                                                starCount: 5,
+                                                rating: reviewsList[index].rate,
+                                                size: 18,
+                                                spacing: 2,
+                                                isReadOnly: true,
+                                                defaultIconData: Icons.star,
+                                                borderColor: Color(0xFF484848),
+                                                color: Colors.yellow[700],
+                                              ),
                                             ),
-                                          ),
-                                          Container(
-                                              margin: EdgeInsets.only(left: 5),
-                                              child: Text('( ',
-                                                  style: TextStyle(
-                                                      color: Colors.white70))),
-                                          Text(
-                                              reviewsList[index]
-                                                  .rate
-                                                  .toStringAsFixed(2),
-                                              style: TextStyle(
-                                                  color: Colors.white)),
-                                          Text(' )',
-                                              style: TextStyle(
-                                                  color: Colors.white70))
-                                        ],
+                                            Container(
+                                                margin: EdgeInsets.only(left: 5),
+                                                child: Text('( ',
+                                                    style: TextStyle(
+                                                        color: Colors.white70))),
+                                            Text(
+                                                reviewsList[index]
+                                                    .rate
+                                                    .toStringAsFixed(2),
+                                                style: TextStyle(
+                                                    color: Colors.white)),
+                                            Text(' )',
+                                                style: TextStyle(
+                                                    color: Colors.white70))
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    Container(
-                                        width: 0.75 * width,
-                                        padding: EdgeInsets.only(
-                                            left: 10,
-                                            top: 5,
-                                            right: 10,
-                                            bottom: 5),
-                                        child: Text(reviewsList[index].review,
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 16)))
-                                  ],
-                                ),
-                              )
-                            ],
+                                      Container(
+                                          width: 0.75 * width,
+                                          padding: EdgeInsets.only(
+                                              left: 10,
+                                              top: 5,
+                                              right: 10,
+                                              bottom: 5),
+                                          child: Text(reviewsList[index].review,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 16)))
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
-            ),
+                      );
+                    }),
+              ),
+      ),
     );
   }
 }
