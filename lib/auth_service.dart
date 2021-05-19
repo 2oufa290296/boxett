12ed61b8208e9d16f62ae2386f0d15d52b807b87
2oufa290296 <83858799+2oufa290296@users.noreply.github.com>
2021-05-19 14:01:45 +0200

    Update auth_service.dart

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -1,8 +1,14 @@
 import 'package:apple_sign_in/apple_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AuthService {
+  final BuildContext context;
+
+  AuthService(this.context);
+
+  
   final _firebaseAuth = FirebaseAuth.instance;
 
   Future<User> signInWithApple({List<Scope> scopes = const []}) async {
@@ -12,6 +18,18 @@ class AuthService {
     // 2. check the result
     switch (result.status) {
       case AuthorizationStatus.authorized:
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          duration: Duration(milliseconds: 2000),
+          backgroundColor: Color(0xFF232323),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 500,
+                child: Text('welcome', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          )));
         final appleIdCredential = result.credential;
         final oAuthProvider = OAuthProvider('apple.com');
         final credential = oAuthProvider.credential(
@@ -28,17 +46,54 @@ class AuthService {
         }
         return firebaseUser;
       case AuthorizationStatus.error:
+       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          duration: Duration(milliseconds: 2000),
+          backgroundColor: Color(0xFF232323),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 500,
+                child: Text(result.error.toString(), style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          )));
         throw PlatformException(
           code: 'ERROR_AUTHORIZATION_DENIED',
           message: result.error.toString(),
         );
 
       case AuthorizationStatus.cancelled:
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          duration: Duration(milliseconds: 2000),
+          backgroundColor: Color(0xFF232323),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 500,
+                child: Text('aborted', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          )));
         throw PlatformException(
+          
           code: 'ERROR_ABORTED_BY_USER',
           message: 'Sign in aborted by user',
         );
       default:
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          duration: Duration(milliseconds: 2000),
+          backgroundColor: Color(0xFF232323),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: 500,
+                child: Text('default', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          )));
         throw UnimplementedError();
     }
   }
