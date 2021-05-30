83385ae83f1b47fc1473dff05d6960bea5da7621
2oufa290296 <83858799+2oufa290296@users.noreply.github.com>
2021-05-30 11:04:17 +0200

    Add files via upload

--- lib/Welcome.dart
+++ lib/Welcome.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,7 +12,24 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
-  FirebaseAuth auth = FirebaseAuth.instance;
+  bool _initialized = false;
+  
+
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _initialized = false;
+      });
+    }
+  }
+
   double width, height;
   // handleUser() async {
   //   Future.delayed(Duration(seconds: 2), () {
@@ -27,34 +45,82 @@ class _WelcomeState extends State<Welcome> {
   //   });
   // }
 
-
   redirecting() async {
-    await Future.delayed(Duration(milliseconds: 2000));
-    SharedPreferences sharedPref = await SharedPreferences.getInstance();
-    String userToken = sharedPref.getString('userToken');
-    String uid = sharedPref.getString('uid');
-    FirebaseMessaging.instance.onTokenRefresh.listen((newToken) async {
-      if (userToken != null && newToken != userToken) {
-        sharedPref.setString('userToken', newToken);
+    if (_initialized) {
+      await Future.delayed(Duration(milliseconds: 1000));
+      SharedPreferences sharedPref = await SharedPreferences.getInstance();
+      String userToken = sharedPref.getString('userToken');
+      String uid = sharedPref.getString('uid');
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+// use the returned token to send messages to users from your custom server
+      String token = await messaging.getToken(
+        vapidKey: "BGpdLRs......",
+      );
+      if (userToken != null && token != userToken) {
+        sharedPref.setString('userToken', token);
         if (uid != null) {
           await FirebaseFirestore.instance
               .collection('Users')
               .doc(uid)
-              .set({'userToken': userToken}, SetOptions(merge: true));
+              .set({'userToken': token}, SetOptions(merge: true));
         }
         print('----Tokenchanged');
       } else {
-        print(newToken);
+        print(token);
       }
-    });
-    
-     Navigator.pushNamed(context, '/redirect');
+      
+      FirebaseMessaging.onMessageOpenedApp.listen((event) {
+        
+       });
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
+
+        if (message.notification != null) {
+          print(
+              'Message also contained a notification: ${message.notification.title}');
+        }
+      });
+
+      if (Platform.isIOS) {
+        await messaging.requestPermission(
+          alert: true,
+          announcement: false,
+          badge: true,
+          carPlay: false,
+          criticalAlert: false,
+          provisional: false,
+          sound: true,
+        );
+      }
+      // FirebaseMessaging.instance.onTokenRefresh.listen((newToken) async {
+      //   if (userToken != null && newToken != userToken) {
+      //     sharedPref.setString('userToken', newToken);
+      //     if (uid != null) {
+      //       await FirebaseFirestore.instance
+      //           .collection('Users')
+      //           .doc(uid)
+      //           .set({'userToken': userToken}, SetOptions(merge: true));
+      //     }
+      //     print('----Tokenchanged');
+      //   } else {
+      //     print(newToken);
+      //   }
+      // });
+
+      Navigator.pushNamed(context, '/redirect');
+    } else {
+      print('error');
+    }
   }
 
   @override
   void initState() {
-    super.initState();
+    initializeFlutterFire();
     redirecting();
+    super.initState();
+
     // handleUser();
   }
 
@@ -62,13 +128,17 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     height = MediaQuery.of(context).size.height;
-    return Scaffold(body:
-    Center(child: Shimmer.fromColors( baseColor: Color.fromRGBO(18, 42, 76, 1),
-            highlightColor: Color.fromRGBO(5, 150, 197, 1),
-            enabled: true,
-                                  child: Text('Boxet',style:TextStyle(fontSize:36,fontFamily:"Lobster",)),
-                ))
-   )
-    ;
+    return Scaffold(
+        body: Center(
+            child: Shimmer.fromColors(
+      baseColor: Color.fromRGBO(18, 42, 76, 1),
+      highlightColor: Color.fromRGBO(5, 150, 197, 1),
+      enabled: true,
+      child: Text('Boxet',
+          style: TextStyle(
+            fontSize: 36,
+            fontFamily: "Lobster",
+          )),
+    )));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@
 import 'package:boxet/LoginState.dart';
 import 'package:boxet/Redirecting.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -31,29 +30,29 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
  
 }
 
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel', // id
-  'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
-  importance: Importance.high,
-);
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//   'high_importance_channel', // id
+//   'High Importance Notifications', // title
+//   'This channel is used for important notifications.', // description
+//   importance: Importance.high,
+// );
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //         AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
+  // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  //   alert: true,
+  //   badge: true,
+  //   sound: true,
+  // );
 
   
   runApp(
@@ -189,6 +188,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    
     mainWidget = HomePage(
       key: homeKey,
     );
