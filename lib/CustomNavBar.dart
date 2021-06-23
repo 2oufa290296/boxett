ac9644bfdc755daf106eba1aa32f1a9d4b9c3ec4
2oufa290296 <83858799+2oufa290296@users.noreply.github.com>
2021-06-23 20:04:21 +0200

    Update CustomNavBar.dart

--- lib/CustomNavBar.dart
+++ lib/CustomNavBar.dart
@@ -278,7 +278,7 @@ class CustomNavBarState extends State<CustomNavBar>
                       0,
                       indexxx == 2
                           ? -(1 - _buttonHide) * 45
-                          : _auth.currentUser!=null && indexxx == 4
+                          : _auth.currentUser != null && indexxx == 4
                               ? -(1 - _buttonHide) * 35
                               : -(1 - _buttonHide) * 40,
                     ),
@@ -308,7 +308,7 @@ class CustomNavBarState extends State<CustomNavBar>
                               // }
                             },
                             child: flareAc)
-                        : _auth.currentUser!=null && indexxx == 4 
+                        : _auth.currentUser != null && indexxx == 4
                             ? Material(
                                 color: widget.buttonBackgroundColor ??
                                     Color.fromRGBO(3, 99, 130, 1),
@@ -357,13 +357,11 @@ class CustomNavBarState extends State<CustomNavBar>
       widget.onTap(index);
     }
     categImg = 'default';
-  
-    if (_auth.currentUser == null && indexxx != 0 && indexxx != 4) {
-        indexxx = index;
+
+    if (_auth.currentUser == null && index != 0 && index!= 4) {
+      indexxx = index;
       if (showw == true) {
-        setState(() {
-          showw = false;
-        });
+        showw = false;
       }
       final newPosition = index / _length;
       setState(() {
@@ -373,7 +371,7 @@ class CustomNavBarState extends State<CustomNavBar>
             duration: widget.animationDuration, curve: widget.animationCurve);
       });
     } else if (_auth.currentUser != null) {
-        indexxx = index;
+      indexxx = index;
       if (indexxx == 4) {
         if (inputController != null) {
           inputController.clear();
