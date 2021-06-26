3e1efe57a26ce25e8e1bb51b52934d31db9e26c8
2oufa290296 <83858799+2oufa290296@users.noreply.github.com>
2021-06-26 07:52:29 +0200

    Update LoginActivity.dart

--- lib/LoginActivity.dart
+++ lib/LoginActivity.dart
@@ -1319,7 +1319,7 @@ Positioned(
                       //     },
                       //   ))
                       // : Container()
-                    ])),            Positioned(right:10,top:20,child:GestureDetector(onTap:(){Navigator.pop(context);},child:Icon(Icons.close,color:Colors.white54,size:20)))
+                    ])),            Positioned(right:10,top:20,child:GestureDetector(onTap:(){Navigator.pop(context);},child:Icon(MdiIcons.closeCircleOutline,color:Colors.white,size:20)))
             ,
           ]),
         ));
